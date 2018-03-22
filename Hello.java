3371e4fd530cfe4caa410748fb825a147b9b71a5
Shefy Gur-Ary <shefy.gurary@intel.com>
2018-03-22 09:27:11 +0200

    Added message Mar22 Ver3

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 	
 		System.out.println("AFTER CHANGE IN NONE JENKINS MACHINE, COMMIT AND PUSH :) ");////
 		for(int i=1; i<=10; i++){
-			System.out.println("Hello World... From  (Mar22 V3) !!! "+i);
+			System.out.println("Hello World... From  (Mar22 V4) !!! "+i);
 		}
 	}
 }
