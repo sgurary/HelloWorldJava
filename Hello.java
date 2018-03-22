cb9998bf73f6d26fec0f7f30c111a40bef2197d4
Shefy Gur-Ary <shefy.gurary@intel.com>
2018-03-22 12:34:54 +0200

    Added message Mar22 Ver5

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello{
 
 	public static void main(String[] args){
 	
-		System.out.println("AFTER CHANGE IN NONE JENKINS MACHINE, COMMIT AND PUSH :) ");////
+		System.out.println("AFTER CHANGE IN NONE JENKINS MACHINE, COMMIT AND PUSH :) V100 ");////
 		for(int i=1; i<=10; i++){
-			System.out.println("Hello World... From  (Mar22 V5) !!! "+i);
+			System.out.println("Hello World... From  (Mar22 V6) !!! "+i);
 		}
 	}
 }
